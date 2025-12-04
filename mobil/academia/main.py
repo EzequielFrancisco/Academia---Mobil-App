194a948cadad570f50240e8bb70205ac9f7ed92a
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-12-04 16:31:33 +0100

    feat/first model

--- mobil/academia/main.py
+++ mobil/academia/main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
+from kivymd.uix.dialog import MDDialog
 
 class SplashScreen(Screen):
     def on_enter(self):
@@ -28,6 +29,20 @@ class HomeScreen(Screen):
 class AcademiaApp(MDApp):
     def build(self):
         return Builder.load_file('screen.kv')
+    
+    def select_card(self, card_id):
+        live = 3
+        print(f"Card {card_id} selected")
+
+        if card_id == "card6":
+            self.show_dialog()
+        else:
+            live -= 1
+            
+    
+    def show_dialog(self):
+        dialog = MDDialog(title="Sucesso", text="Correcto", size_hint=(0.8, None), height=200)
+        dialog.open()
 
 
 if __name__ == "__main__":
